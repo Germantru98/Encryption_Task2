1139d2bb8a83a0e99a55c0d936875883271da41a
Germantru98 <germantruhanov@gmail.com>
2019-09-13 19:40:51 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataPharser dp = new DataPharser();
+            string str = dp.getMessage("Message.txt");
+            var res = str
+                .GroupBy(c => c)
+                .ToDictionary(g => g.Key, g => g.Count());
+            foreach (var item in res)
+            {
+                Console.WriteLine("item - {0}, frequaecy-{1:0.###}" ,item,(double)item.Value/str.Length);
+            }
+        }
+    }
+}
